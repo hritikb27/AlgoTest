3ed7358d26288150029b54183e499d787526532b
unknown <hritikb90@gmail.com>
2022-09-27 12:27:51 +0530

    added dispatch functions for options select values

--- src/components/RenderLegs/Options.js
+++ src/components/RenderLegs/Options.js
@@ -16,7 +16,25 @@ const Options = ({ leg }) => {
             if (leg.id === id) {
                 return {
                     ...leg,
-                    value: value,
+                    totalLot: value,
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
+    const handleTSLselect = (checked, id) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    TSL: {
+                        ...leg.TSL,
+                        checked: !checked,
+                    }
                 }
             } else {
                 return leg
@@ -34,24 +52,6 @@ const Options = ({ leg }) => {
                     ...leg,
                     SM: {
                         ...leg.SM,
-                        checked: !checked,
-                    }
-                }
-            } else {
-                return leg
-            }
-        })
-
-        dispatch(updateLeg(newLegs))
-    }
-
-    const handleTSLselect = (checked, id) => {
-        const newLegs = legs.map(leg => {
-            if (leg.id === id) {
-                return {
-                    ...leg,
-                    TSL: {
-                        ...leg.TSL,
                         checked: !checked,
                     }
                 }
@@ -116,6 +116,24 @@ const Options = ({ leg }) => {
         dispatch(updateLeg(newLegs))
     }
 
+    const handleSMValue = (value, id) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    SM: {
+                        ...leg.SM,
+                        value: value,
+                    }
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
     const handleTSLtype = (value, id) => {
         const newLegs = legs.map(leg => {
             if (leg.id === id) {
@@ -134,9 +152,83 @@ const Options = ({ leg }) => {
         dispatch(updateLeg(newLegs))
     }
 
+    const handlePositionChange = (id, value) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    position: value
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
+    const handleOptionTypeChange = (id, value) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    optionType: value
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
+    const handleExpiryChange = (id, value) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    expiry: value
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
+    const handleStrikeCriteriaChange = (id, value) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    strikeCriteria: value
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
+    const handleStrikeTypeChange = (id, value) => {
+        const newLegs = legs.map(leg => {
+            if (leg.id === id) {
+                return {
+                    ...leg,
+                    strikeType: value
+                }
+            } else {
+                return leg
+            }
+        })
+
+        dispatch(updateLeg(newLegs))
+    }
+
     const copyLeg = (id) => {
         const copiedLeg = legs.find(leg => leg.id === id)
-
         const newLeg = {
             id: uuid(),
             type: copiedLeg.type,
@@ -149,49 +241,45 @@ const Options = ({ leg }) => {
             SM: copiedLeg.SM,
             TSL: copiedLeg.TSL
         }
-
+        console.log(newLeg)
         dispatch(addLeg(newLeg))
     }
 
     const deleteLeg = (id) => {
-        const newLegs =  legs.filter((item) => item.id !== id);
+        const newLegs = legs.filter((item) => item.id !== id);
         dispatch(updateLeg(newLegs))
     }
 
     return <div className="flex flex-col gap-5 bg-[#efefef] p-10 rounded-xl relative" key={leg.id}>
-        <AiFillDelete className="absolute right-[-5px] top-[-7px] cursor-pointer" onClick={()=>deleteLeg(leg.id)} />
-        <FaRegCopy className="absolute right-[-7px] top-[18px] cursor-pointer" onClick={()=>copyLeg(leg.id)} />
+        <AiFillDelete className="absolute right-[-5px] top-[-7px] cursor-pointer" onClick={() => deleteLeg(leg.id)} />
+        <FaRegCopy className="absolute right-[-7px] top-[18px] cursor-pointer" onClick={() => copyLeg(leg.id)} />
         <div className="flex gap-5 justify-center">
             <label>Lot</label>
             <div className="relative">
-                <input type='number' min='1' value={leg.value} onChange={(event) => handleLotValue(event.target.value, leg.id)} className='appearance-none border border-[#f6f6f6] rounded-xl h-[25px] w-[80px] px-3 py-[2px] text-xs ' />
+                <input type='number' min='1' value={leg.totalLot} onChange={(event) => handleLotValue(event.target.value, leg.id)} className='appearance-none border border-[#f6f6f6] rounded-xl h-[25px] w-[80px] px-3 py-[2px] text-xs ' />
                 <IoChevronUp className='absolute right-3 top-0 text-[#C7C7C7]' />
                 <IoChevronDown className='absolute right-3 bottom-0 text-[#C7C7C7]' />
             </div>
-            <select defaultValue={leg.position} className="bg-[#375a9e] text-white" >
+            <select defaultValue={leg.position} className="bg-[#375a9e] text-white" onChange={(event) => handlePositionChange(leg.id, event.target.value)} >
                 <option>Sell</option>
                 <option>Buy</option>
             </select>
-            <select defaultValue={leg.optionType} className="bg-[#375a9e] text-white">
+            <select defaultValue={leg.optionType} className="bg-[#375a9e] text-white" onChange={(event) => handleOptionTypeChange(leg.id, event.target.value)} >
                 <option>Call</option>
                 <option>Put</option>
             </select>
-            <select defaultValue={leg.expiry} className="bg-[#375a9e] text-white">
+            <select defaultValue={leg.expiry} className="bg-[#375a9e] text-white" onChange={(event) => handleExpiryChange(leg.id, event.target.value)} >
                 <option>Weekly</option>
                 <option>Monthly</option>
             </select>
             <label>Select Strike</label>
-            <select defaultValue={leg.expiry} className="bg-[#375a9e] text-white">
+            <select defaultValue={leg.strikeCriteria} className="bg-[#375a9e] text-white" onChange={(event) => handleStrikeCriteriaChange(leg.id, event.target.value)} >
                 <option>Strike Type</option>
                 <option>Premium Range</option>
                 <option>Closest Premium</option>
                 <option>Straddle Width</option>
             </select>
-            <select defaultValue={leg.strikeType} className="bg-[#375a9e] text-white">
-                <option>Weekly</option>
-                <option>Monthly</option>
-            </select>
-            <select defaultValue={leg.position} className="bg-[#375a9e] text-white" >
+            <select defaultValue={leg.strikeType} className="bg-[#375a9e] text-white" onChange={(event) => handleStrikeTypeChange(leg.id, event.target.value)} >
                 {options.map(option => {
                     return <option>{option}</option>
                 })}
@@ -217,7 +305,7 @@ const Options = ({ leg }) => {
                     </select>
 
                     <div className="relative">
-                        <input type='number' min='1' value={leg.SM.value} onChange={(event) => handleLotValue(event.target.value, leg.id)} className='appearance-none border border-[#f6f6f6] rounded-xl h-[25px] w-[80px] px-3 py-[2px] text-xs ' />
+                        <input type='number' min='1' value={leg.SM.value} onChange={(event) => handleSMValue(event.target.value, leg.id)} className='appearance-none border border-[#f6f6f6] rounded-xl h-[25px] w-[80px] px-3 py-[2px] text-xs ' />
                         <IoChevronUp className='absolute right-3 top-0 text-[#C7C7C7]' />
                         <IoChevronDown className='absolute right-3 bottom-0 text-[#C7C7C7]' />
                     </div>
